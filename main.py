7d300285cd5959a2d81c4fdb68628d187cb4d84b
niels demeyer <niels.demeyer99@gmail.com>
2024-02-14 10:37:30 +0100

    mvp runs :)

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from src.scraper import WikipediaScraper
 
 
-def main():
-    scraper = WikipediaScraper()
-    countries = scraper.get_countries()
+# def main():
+#     scraper = WikipediaScraper()
+#     countries = scraper.get_countries()
 
-    for country in countries:
-        scraper.get_leaders(country)
+#     for country in countries:
+#         scraper.get_leaders(country)
 
-    scraper.to_json_file("leaders_data.json")
+#     scraper.to_json_file("leaders_data.json")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+countries = WikipediaScraper().get_countries()
+print(len(countries))
